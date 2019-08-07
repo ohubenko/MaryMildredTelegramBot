d38c3ce3bb027b06a3640ab6d0fa2ddfb533aec1
MrEagle <sasha0420011@gmail.com>
2019-08-07 13:58:57 +0300

    New massage to user

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.reply_to(message,
+                 'Привет, ' + message.from_user.first_name +
+                 '. Теперь ты будешь поулчать уведомления о начале стрима, а также о новых постах в группе VK')
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message,
+                 "Извини, я ищё не умею отвечать на обычные сообщения. Но скоро я смогу с тобой полноценно общаться")
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -28,7 +31,7 @@ def getMessage():
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://marymildred-bot.herokuapp.com/' + TOKEN)
-    return "!", 200
+    return "Bot has been work!", 200
 
 
 if __name__ == "__main__":
