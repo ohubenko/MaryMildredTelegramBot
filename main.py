9d639a1a95ca22d6f115f1306ed07267ab8727f5
MrEagle <sasha0420011@gmail.com>
2019-08-08 10:25:46 +0300

    Test #

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def echo_message(message):
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    print(bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]))
     return "!", 200
 
 
