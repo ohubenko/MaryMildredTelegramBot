fe72e69a78627139bf3e1cd4c279c80082864cde
MrEagle <sasha0420011@gmail.com>
2019-10-13 00:04:30 +0300

    Try fix problem with write db

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ server = Flask(__name__)
 # MongoDB_MLab
 # mongodb_url = str(os.getenv('mongodb_url'))
 mongodb_url = "mongodb://MildredBot:SaMp4721@ds261377.mlab.com:61377/heroku_03snt0h5"
-client = pymongo.MongoClient(mongodb_url, retryWrites=True)
+client = pymongo.MongoClient(mongodb_url, retryWrites=False)
 db = client.get_database(str(os.getenv('db_name')))
 records = db["users"]
 
